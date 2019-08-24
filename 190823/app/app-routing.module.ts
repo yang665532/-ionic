ead5dc2d4bd60593913b2e26c486dd27c8793c46
LiouJean <49053988+LiouJean@users.noreply.github.com>
2019-08-25 00:33:14 +0800

    Add register info to Firebase database

---
+++ 190823/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
+
+
+
+
+const routes: Routes = [
+  //{ path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule'
+    , canActivate: [AuthGuard] },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' },
+  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  { path: 'info', loadChildren: './info/info.module#InfoPageModule' },
+  { path: 'faq', loadChildren: './faq/faq.module#FaqPageModule' },
+  { path: 'help', loadChildren: './help/help.module#HelpPageModule' },
+  { path: 'setting', loadChildren: './setting/setting.module#SettingPageModule' },
+  { path: 'acup', loadChildren: './acup/acup.module#AcupPageModule' },
+  { path: 'market', loadChildren: './market/market.module#MarketPageModule' },
+  { path: 'course', loadChildren: './course/course.module#CoursePageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'reset-password', loadChildren: './reset-password/reset-password.module#ResetPasswordPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
