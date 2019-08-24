386ae03e409a629045b5037abd47844a7348c67b
LiouJean <49053988+LiouJean@users.noreply.github.com>
2019-08-24 23:54:33 +0800

    Can register new users and login
    method using Google Firebase

---
+++ 190821/setting/setting.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-setting',
+  templateUrl: './setting.page.html',
+  styleUrls: ['./setting.page.scss'],
+})
+export class SettingPage implements OnInit {
+
+  constructor(public navCtrl: NavController,
+              private authService: AuthService) { }
+
+  goHome(): void{
+    this.navCtrl.navigateRoot('/tabs');
+  }
+  logout(){
+    this.authService.logoutUser()
+    .then(res => {
+      console.log(res);
+      this.navCtrl.navigateBack('');
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
