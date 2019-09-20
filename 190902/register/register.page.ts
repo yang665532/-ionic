483839358658ad01dbde285634b480e7b0929d1e
Jean Liou <49053988+LiouJean@users.noreply.github.com>
2019-09-20 21:53:27 +0800

    Able to send verification email to users
    but can't verify if the users have verified the email or not

---
+++ 190902/register/register.page.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from './../services/auth.service';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+//import { AngularFireAuth } from '@angular/fire/auth';
+//import { AngularFireAuth } from '@angular/fire/auth/auth';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  public signupForm: FormGroup;
+  public loading: any;
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthService,
+    //private afAuth: AngularFireAuth,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.signupForm = this.formBuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.required, Validators.email]),
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.minLength(6), Validators.required]),
+      ],
+    });
+  }
+
+  async signupUser(signupForm: FormGroup): Promise<void> {
+    if (!signupForm.valid) {
+      console.log(
+        'Need to complete the form, current value: ', signupForm.value
+      );
+    } else {
+      const email: string = signupForm.value.email;
+      const password: string = signupForm.value.password;
+  
+      this.authService.registerUser(email, password).then(
+        () => {
+          //this.afAuth.auth.currentUser.sendEmailVerification().then(() =>
+          this.loading.dismiss().then(async () => {
+            const alert = await this.alertCtrl.create({
+              header: "Success!",
+              message: "Registration Successful! Please Verify Your Email.",
+              buttons: [{ text: 'Ok', role: 'cancel' }]
+            });
+            this.router.navigateByUrl('verify-email');
+            await alert.present();
+          });
+        },
+        error => {
+          this.loading.dismiss().then(async () => {
+            const alert = await this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: 'Ok', role: 'cancel' }],
+            });
+            await alert.present();
+          });
+        }
+      );
+      this.loading = await this.loadingCtrl.create();//this.router.navigateByUrl('tabs');
+      await this.loading.present();
+    }
+  }
+
+  ngOnInit(){
+    // this.validations_form = this.formBuilder.group({
+    //   email: new FormControl('', Validators.compose([
+    //     Validators.required,
+    //     Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    //   ])),
+    //   password: new FormControl('', Validators.compose([
+    //     Validators.minLength(5),
+    //     Validators.required
+    //   ])),
+    // });
+  }
+ 
+  // tryRegister(value){
+  //   this.authService.registerUser(value)
+  //    .then(res => {
+  //      console.log(res);
+  //      this.errorMessage = "";
+  //      this.successMessage = "帳號註冊成功，請重新登入";
+  //    }, err => {
+  //      console.log(err);
+  //      this.errorMessage = err.message; //改中文(email address already in use)
+  //      this.successMessage = "";
+  //    })
+  // }
+ 
+  // goLoginPage(){
+  //   this.navCtrl.navigateRoot('/login');
+  // }
+
+}
