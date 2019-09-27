f806ea4a9e1a7c7e6ac94325813b88ea72f53a79
Jean Liou <49053988+LiouJean@users.noreply.github.com>
2019-09-27 23:18:52 +0800

    Add entrance with buttons of login and register

---
+++ 190927/entrance/entrance.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-entrance',
+  templateUrl: './entrance.page.html',
+  styleUrls: ['./entrance.page.scss'],
+})
+export class EntrancePage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    this.router.navigateByUrl('login');
+  }
+
+  register(){
+    this.router.navigateByUrl('register');
+  }
+
+}
